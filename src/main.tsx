5b2dc53b9b53557f3160fbfdab2186f015e4e0e3
salmaly-ui <salmalaouy133@gmail.com>
2026-05-15 13:14:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+// @ts-ignore
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 import { HelmetProvider } from "react-helmet-async";
